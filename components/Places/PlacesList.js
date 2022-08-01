9cf453afa0188a3826fdd160b03f62ec6f02c9c9
Ayush <ayushagarwal.dev@gmail.com>
2022-08-01 22:38:52 +0530

    add Add Place screen and Navigation

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, Text } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import PlaceItem from "./PlaceItem";
 
