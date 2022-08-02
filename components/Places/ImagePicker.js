8124b239a645bd67b293e8c0073b085a9036af46
Ayush <ayushagarwal.dev@gmail.com>
2022-08-02 23:27:12 +0530

    configure for ios and add verify permission fn

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,9 +1,38 @@
 import React from "react";
-import { Button, StyleSheet, View } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
+import { Alert, Button, StyleSheet, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
 
 export default function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions",
+        "You need to grant camera permission to use this app."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
   async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
