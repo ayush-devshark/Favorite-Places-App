9fc48e2da480084598d069635f0a1a950b071f4b
Ayush <ayushagarwal.dev@gmail.com>
2022-08-02 23:41:09 +0530

    add image preview elements

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { Alert, Button, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Button, Image, StyleSheet, View, Text } from "react-native";
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
 } from "expo-image-picker";
 
+import { COLORS } from "../../constants/colors";
+
 export default function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState(null);
+
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
@@ -38,15 +42,37 @@ export default function ImagePicker() {
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+    setPickedImage(image);
+  }
+
+  let imagePreview = <Text>No Image taken yet</Text>;
+
+  if (pickedImage) {
+    imagePreview = (
+      <Image style={styles.image} source={{ uri: pickedImage.uri }} />
+    );
   }
 
   return (
     <View>
-      <View></View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
       <Button title="Take Image" onPress={takeImageHandler} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: COLORS.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+  },
+});
