9a787834e070784f026a867f24bd79363f984a82
Ayush <ayushagarwal.dev@gmail.com>
2022-08-08 23:52:49 +0530

    add picked location map interactions and markers

--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
 export default function Map() {
+  const [selectedLocation, setSelectedLocation] = useState();
   const region = {
     latitude: 37.78,
     longitude: -122.43,
@@ -9,7 +11,30 @@ export default function Map() {
     longitudeDelta: 0.0421,
   };
 
-  return <MapView style={styles.map} initialRegion={region} />;
+  function selectLocationHandler(event) {
+    // console.log(event);
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+    setSelectedLocation({ lat, lng });
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation?.lat,
+            longitude: selectedLocation?.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 const styles = StyleSheet.create({
