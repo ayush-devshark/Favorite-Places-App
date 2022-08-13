777b6693225b22ee0ee5e32198f5831065f05e3b
Ayush <ayushagarwal.dev@gmail.com>
2022-08-13 17:09:59 +0530

    fetch places from local SQL database

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -6,7 +6,6 @@ import { insertPlace } from "../util/database";
 export default function AddPlace({ navigation }) {
   async function createPlaceHandler(place) {
     await insertPlace(place);
-    navigation.navigate("AllPlaces", { place: place });
   }
 
   return <PlaceForm onCreatePlace={createPlaceHandler} />;
