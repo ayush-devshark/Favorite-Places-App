d7072da7cf964cfc65d9a4a8ec29fda0589f2cd8
Ayush <ayushagarwal.dev@gmail.com>
2022-08-13 16:51:55 +0530

    add insert place to database logic

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,9 +1,11 @@
 import { StyleSheet } from "react-native";
 import React from "react";
 import PlaceForm from "../components/Places/PlaceForm";
+import { insertPlace } from "../util/database";
 
 export default function AddPlace({ navigation }) {
-  function createPlaceHandler(place) {
+  async function createPlaceHandler(place) {
+    await insertPlace(place);
     navigation.navigate("AllPlaces", { place: place });
   }
 
